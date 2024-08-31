93d02804b6895b23a69b3d4b553a0ed988ff1314
Sourabh Patel <getmeapencil@gmail.com>
2024-08-31 19:56:39 +0530

    make table scrollable with sticky header

--- client/src/pages/dashboard/components/InvitesTable.jsx
+++ client/src/pages/dashboard/components/InvitesTable.jsx
@@ -1,52 +1,55 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { Button } from "../../../components/ui/button";
+import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import inviteData from "../../../assets/inviteData.json";
-
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export const InvitesTable = () => {
   return (
-    <Card className={`h-80 overflow-y-auto`}>
-      <CardHeader className="px-7">
-        <CardTitle>Room Invitations</CardTitle>
-        <CardDescription>Recent rooms invitations recieved.</CardDescription>
+    <Card>
+      <CardHeader>
+        <CardTitle>Invites</CardTitle>
+        <CardDescription>Rooms you&apos;ve been invited to.</CardDescription>
       </CardHeader>
       <CardContent>
-        <Table>
-          <TableHeader className="sticky top-0 z-10 bg-white">
-            <TableRow className="">
-              <TableHead>Room</TableHead>
-              <TableHead className="sm:table-cell">Role</TableHead>
-              <TableHead className="text-center sm:table-cell">Total Members</TableHead>
-              <TableHead className="text-center sm:table-cell">Action</TableHead>
-            </TableRow>
-          </TableHeader>
-
-          <TableBody>
-            {inviteData.map((data) => (
-              <TableRow key={data.id} className="bg-accent">
-                <TableCell className="flex items-center">
-                  <div>
+        <ScrollArea className="h-72 rounded-md border">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="sm:table-cell">Room</TableHead>
+                <TableHead className="text-center sm:table-cell">Role</TableHead>
+                <TableHead className="text-center sm:table-cell">Total Members</TableHead>
+                <TableHead className="text-center sm:table-cell">Action</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {inviteData.map((data) => (
+                <TableRow key={data.id}>
+                  <TableCell className="flex items-center gap-2">
                     <Avatar>
                       <AvatarImage src="https://github.com/shadcn.png" />
                       <AvatarFallback>CN</AvatarFallback>
                     </Avatar>
-                  </div>
-                  <div className="px-2 font-medium">{data.roomName}</div>
-                </TableCell>
-                <TableCell className="sm:table-cell">{data.role}</TableCell>
-                <TableCell className="text-center sm:table-cell">{data.totalMembers} Members</TableCell>
-                <TableCell className="text-center sm:table-cell">
-                  <Button className="mx-1 bg-green-500 hover:bg-green-600">Accept</Button>
-                  <Button variant="destructive" className="hover:bg-red-600">
-                    Reject
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+                    <div className="font-medium">{data.roomName}</div>
+                  </TableCell>
+                  <TableCell className="text-center sm:table-cell">{data.role}</TableCell>
+                  <TableCell className="text-center sm:table-cell">{data.totalMembers} Members</TableCell>
+                  <TableCell className="text-center sm:table-cell">
+                    <div className="flex place-content-center gap-2">
+                      <Button variant="outline" className="border-green-300 hover:bg-green-300">
+                        Accept
+                      </Button>
+                      <Button variant="outline" className="border-red-300 hover:bg-red-300">
+                        Reject
+                      </Button>
+                    </div>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </ScrollArea>
       </CardContent>
     </Card>
   );
