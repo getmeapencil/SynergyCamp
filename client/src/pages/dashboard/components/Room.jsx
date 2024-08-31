282a8068ce57903f1a3e5009154e2afb1b5cf378
Sourabh Patel <getmeapencil@gmail.com>
2024-08-31 20:06:41 +0530

    use @ for relative path

--- client/src/pages/dashboard/components/Room.jsx
+++ client/src/pages/dashboard/components/Room.jsx
@@ -1,6 +1,6 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../../../components/ui/card";
-import { Input } from "../../../components/ui/input";
-import { Button } from "../../../components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export const Room = () => {
   return (
