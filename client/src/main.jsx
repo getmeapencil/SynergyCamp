2bc4e25c96f048706e6364c87bbb29b22b178a4c
Yogitheboss <yograjrjpt123@gmail.com>
2024-08-31 13:21:51 +0530

    backend setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+const apiUrl = "147321809761-8vpt8ec4hgabcp0s4j98shiu1s6too0s.apps.googleusercontent.com";
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={apiUrl}>
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
-)
+);
