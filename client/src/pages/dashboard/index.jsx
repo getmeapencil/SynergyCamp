8c49f28f2b3aa80d1474adc54a55f688bdd916cc
Sourabh Patel <getmeapencil@gmail.com>
2024-09-01 00:21:06 +0530

    add topbar layout

--- client/src/pages/dashboard/index.jsx
+++ client/src/pages/dashboard/index.jsx
@@ -1,18 +1,33 @@
 import { Barchart } from "./components/Barchart";
 import { Room } from "./components/Room";
 import { RoomsData } from "./components/RoomsData";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export const Dashboard = () => {
   return (
     <>
-      <div className="flex h-screen flex-col gap-4 p-4 sm:flex-row">
-        <div className="flex flex-col gap-4 sm:w-1/3">
-          <Room />
-          <Barchart />
+      <div className="flex h-screen flex-col">
+        <div className="flex items-center justify-between border border-b p-4">
+          <span className="text-4xl font-extrabold">MindMesh</span>
+          {/* Add DropDown here - Show Name, username, Switch for lightmode and dark, logout  */}
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
         </div>
-        <div className="flex-1">
-          <RoomsData />
-        </div>
+        <ScrollArea>
+          <div className="flex gap-4 p-4 sm:flex-row">
+            <div className="flex flex-col gap-4 sm:w-1/3">
+              <Room />
+              <Barchart />
+            </div>
+            <div className="flex flex-1 flex-col gap-4">
+              <RoomsData />
+              {/* More components to come here */}
+            </div>
+          </div>
+        </ScrollArea>
       </div>
     </>
   );
