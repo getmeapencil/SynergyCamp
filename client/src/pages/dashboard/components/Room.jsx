c6773f15026e5855c9409002b196bf69420d4161
Sourabh Patel <getmeapencil@gmail.com>
2024-08-31 19:58:33 +0530

    keep simple layout

--- client/src/pages/dashboard/components/Room.jsx
+++ client/src/pages/dashboard/components/Room.jsx
@@ -4,16 +4,16 @@ import { Button } from "../../../components/ui/button";
 
 export const Room = () => {
   return (
-    <div className="mt-2">
+    <div className="w-full">
       <Card className="min-w-sm">
-        <CardHeader className="p-3 pb-0">
+        <CardHeader>
           <CardTitle>Your Room</CardTitle>
           <CardDescription>Create a room of your own and host a study session.</CardDescription>
-          <p className="font-bold">Room Name</p>
         </CardHeader>
-        <CardContent className="p-3">
+        <CardContent>
+          <p className="font-bold">Room Name</p>
           <form className="flex flex-col gap-2">
-            <Input type="text" placeholder="Enter your text" aria-label="Text input" />
+            <Input type="text" placeholder="Type room name here" aria-label="Text input" />
             <Button type="submit">Create New Room</Button>
           </form>
         </CardContent>
