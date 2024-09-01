07160197a41a11ff0f3946d94c877e6b7f183ff1
Suruchi Pandey <suchikasdol@gmail.com>
2024-09-01 14:33:16 +0530

    Auth UI Updated

--- client/src/Pages/Auth/index.jsx
+++ client/src/Pages/Auth/index.jsx
@@ -1,46 +1,30 @@
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 
 export function Auth() {
   return (
-    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
-      <div className="hidden bg-black lg:block flex flex-col justify-between p-8">
-        <h1 className="text-white text-3xl font-bold">Mind Mesh</h1>
-        <p className="text-white text-lg font-semibold">Boost your productivity together</p>
+    <div className="w-full h-screen lg:grid lg:grid-cols-2">
+      <div className="hidden bg-foreground lg:flex flex-col justify-between p-6">
+        <h1 className="text-white text-2xl font-bold">Mind Mesh</h1>
+        <div className="flex-grow"></div> {/* Empty div to push content to the bottom */}
+        <p className="text-white text-base font-medium">
+          Boost Your Productivity Together -
+          <div>Study and Work with Friends in Real-Time</div>
+        </p>
       </div>
-      <div className="flex items-center justify-center py-12">
-        <div className="mx-auto grid w-[350px] gap-6">
-          <div className="grid gap-2 text-center">
-            <h1 className="text-3xl font-bold">Login</h1>
-            <p className="text-balance text-muted-foreground">
-              Enter your email below to login to your account
-            </p>
-          </div>
-          <div className="grid gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-              </div>
-              <Input id="password" type="password" required />
-            </div>
+      <div className="flex items-center justify-center p-6">
+        <div className="mx-auto grid w-[300px] gap-4">
+          <div className="grid gap-1 text-center">
+            <h5 className="text-lg font-medium">Sign up / Sign In</h5>
+    
             <Button type="submit" className="w-full">
-              Login
+            <img 
+                
+                alt="Google" 
+                className="h-6 w-auto" 
+              />
             </Button>
-            <Button variant="outline" className="w-full">
-              Login with Google
-            </Button>
+            
           </div>
-          
         </div>
       </div>
     </div>
