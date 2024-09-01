4772ce98bd562051a1864bf1e51fc293a4c630f9
Yogitheboss <yograjrjpt123@gmail.com>
2024-09-01 19:54:01 +0530

    adding a image in left side

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -3,7 +3,7 @@ import { FcGoogle } from "react-icons/fc";
 export const Auth = () => {
   return (
     <div className="h-screen w-full lg:grid lg:grid-cols-2">
-      <div className="hidden flex-col justify-between bg-foreground p-6 lg:flex">
+      <div className="hidden flex-col justify-between bg-[url('/src/assets/study_group.jpeg')] p-6 lg:flex">
         <h1 className="text-4xl font-extrabold text-white">MindMesh</h1>
         <div className="flex-grow"></div> {/* Empty div to push content to the bottom */}
         <p className="text-3xl font-medium text-white">
@@ -12,7 +12,7 @@ export const Auth = () => {
       </div>
       <div className="flex items-center justify-center p-6">
         <div className="mx-auto grid w-[300px] gap-4">
-          <div className="grid gap-3 text-center ">
+          <div className="grid gap-3 text-center">
             <div className="flex flex-col gap-2">
               <div className="flex justify-center text-2xl md:text-5xl lg:text-5xl">
                 <h1>Welcome to MindMesh</h1>
