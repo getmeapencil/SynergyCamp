27284cc909d25506efb2435b232aa35e0fb94a38
Sourabh Patel <getmeapencil@gmail.com>
2024-09-01 21:16:37 +0530

    add todo stat

--- client/src/pages/dashboard/components/TodoOverview.jsx
+++ client/src/pages/dashboard/components/TodoOverview.jsx
@@ -4,9 +4,12 @@ import { Checkbox } from "@/components/ui/checkbox";
 export const TodoOverview = () => {
   return (
     <Card className="w-1/3">
-      <CardHeader>
-        <CardTitle>ToDo</CardTitle>
-        <CardDescription>ToDo list from all rooms.</CardDescription>
+      <CardHeader className="flex flex-row justify-between">
+        <div className="flex flex-col space-y-1.5">
+          <CardTitle>ToDo</CardTitle>
+          <CardDescription>ToDo list from all rooms.</CardDescription>
+        </div>
+        <div className="text-5xl">2/5</div>
       </CardHeader>
       <CardContent className="flex flex-col gap-2">
         <div className="items-top flex space-x-2">
@@ -22,10 +25,10 @@ export const TodoOverview = () => {
           </div>
         </div>
         <div className="items-top flex space-x-2">
-          <Checkbox id="terms1" />
+          <Checkbox id="terms2" />
           <div className="grid gap-1.5 leading-none">
             <label
-              htmlFor="terms1"
+              htmlFor="terms2"
               className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
             >
               Work on backend of OB
