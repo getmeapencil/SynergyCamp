166d2e128d3c9abcf7380a09764fa4912808eade
Sourabh Patel <getmeapencil@gmail.com>
2024-09-01 16:25:18 +0530

    run prettier

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,13 @@ import { Auth } from "@/pages/auth";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <>
-    <Routes>
-      <Route path="/auth" element = {<Auth/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
