5841c8e66d97eaf17daef05f39f7ecf192fb3139
Sourabh Patel <getmeapencil@gmail.com>
2024-09-05 02:12:09 +0530

    previous icons were fine

--- client/src/pages/dashboard/components/EmptyTable.jsx
+++ client/src/pages/dashboard/components/EmptyTable.jsx
@@ -1,11 +1,11 @@
 import { BsHouseExclamation } from "react-icons/bs";
 import { LuMailWarning } from "react-icons/lu";
-import { MdSearchOff } from "react-icons/md";
+
 export const EmptyTable = ({ variant, text }) => {
   return (
     <div className="grid h-72 place-content-center gap-4 text-center">
       <span className="grid place-content-center text-9xl text-muted-foreground">
-        {variant === "room" ? <BsHouseExclamation /> : variant === "emptysearch" ? <MdSearchOff /> : <LuMailWarning />}
+        {variant === "room" ? <BsHouseExclamation /> : <LuMailWarning />}
       </span>
       <p className="text-center text-xl text-muted-foreground">{text}</p>
     </div>
