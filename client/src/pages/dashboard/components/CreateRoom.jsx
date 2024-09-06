69495196bda3552fdf83e32d80450c9f34201a1a
Sourabh Patel <getmeapencil@gmail.com>
2024-09-06 23:51:52 +0530

    rename and add typewriter effect

--- client/src/pages/dashboard/components/CreateRoom.jsx
+++ client/src/pages/dashboard/components/CreateRoom.jsx
@@ -1,14 +1,25 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import Typewriter from "typewriter-effect";
 
-export const Room = () => {
+export const CreateRoom = () => {
   return (
     <div className="w-full">
       <Card className="min-w-sm">
         <CardHeader>
           <CardTitle>Your Room</CardTitle>
-          <CardDescription>Create a room of your own and host a study session.</CardDescription>
+          <CardDescription className="flex gap-1">
+            <span>Create a room of your own and host a</span>
+            <Typewriter
+              options={{
+                strings: ["study", "work"],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+            <span>session.</span>
+          </CardDescription>
         </CardHeader>
         <CardContent>
           <p className="font-bold">Room Name</p>
