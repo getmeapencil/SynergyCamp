e22dbf3f5221cf52134313ee4391909a3828e4dc
Sourabh Patel <getmeapencil@gmail.com>
2024-09-06 23:42:48 +0530

    refactor

--- client/src/pages/dashboard/index.jsx
+++ client/src/pages/dashboard/index.jsx
@@ -1,21 +1,22 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useTheme } from "@/components/theme-provider";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { useUserStore } from "@/store/user";
 import { Barchart } from "./components/Barchart";
 import { DropdownAvatar } from "./components/DropdownAvatar";
 import { Room } from "./components/Room";
 import { RoomsData } from "./components/RoomsData";
-import { ScrollArea } from "@/components/ui/scroll-area";
 import { TodoOverview } from "./components/TodoOverview";
 import { Heatmap } from "./components/Heatmap";
-
-import { useUserStore } from "@/store/user";
-import { useEffect } from "react";
 import LogoBlack from "/logo-black.svg";
 import LogoWhite from "/logo-white.svg";
-import { useTheme } from "@/components/theme-provider";
-import { useNavigate } from "react-router-dom";
+
 export const Dashboard = () => {
   const { user } = useUserStore();
   const { theme } = useTheme();
   const navigate = useNavigate();
+
   useEffect(() => {
     // fetch user data
     if (!user) {
@@ -28,41 +29,40 @@ export const Dashboard = () => {
           }
         });
     }
-  }, [user]);
+  }, [user, navigate]);
+
   if (!user) {
     return null;
   }
+
   return (
-    <>
-      <div className="flex h-screen flex-col">
-        <div className="flex items-center justify-between border border-b p-4">
-          <span className="flex gap-2 text-4xl font-extrabold">
-            <img
-              src={theme === "light" ? LogoBlack : LogoWhite}
-              alt="MindMesh"
-              className="grid aspect-square w-8 place-content-center"
-            />
-            MindMesh
-          </span>
-          {/* Add DropDown here - Show Name, username, Switch for lightmode and dark, logout  */}
-          <DropdownAvatar user={user} />
-        </div>
-        <ScrollArea>
-          <div className="flex gap-4 p-4 sm:flex-row">
-            <div className="flex flex-col gap-4 sm:w-1/3">
-              <Room />
-              <Barchart />
-            </div>
-            <div className="flex flex-1 flex-col gap-4">
-              <RoomsData />
-              <div className="flex flex-1 gap-4">
-                <TodoOverview />
-                <Heatmap />
-              </div>
+    <div className="flex h-screen flex-col">
+      <div className="flex items-center justify-between border border-b p-4">
+        <span className="flex gap-2 text-4xl font-extrabold">
+          <img
+            src={theme === "light" ? LogoBlack : LogoWhite}
+            alt="MindMesh"
+            className="grid aspect-square w-8 place-content-center"
+          />
+          MindMesh
+        </span>
+        <DropdownAvatar user={user} />
+      </div>
+      <ScrollArea>
+        <div className="flex gap-4 p-4 sm:flex-row">
+          <div className="flex flex-col gap-4 sm:w-1/3">
+            <Room />
+            <Barchart />
+          </div>
+          <div className="flex flex-1 flex-col gap-4">
+            <RoomsData />
+            <div className="flex flex-1 gap-4">
+              <TodoOverview />
+              <Heatmap />
             </div>
           </div>
-        </ScrollArea>
-      </div>
-    </>
+        </div>
+      </ScrollArea>
+    </div>
   );
 };
