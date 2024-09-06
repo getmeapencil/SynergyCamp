5170c47026d140f2565e72208bc59fc688447e7f
Sourabh Patel <getmeapencil@gmail.com>
2024-09-06 23:52:05 +0530

    change component name

--- client/src/pages/dashboard/index.jsx
+++ client/src/pages/dashboard/index.jsx
@@ -5,7 +5,7 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { useUserStore } from "@/store/user";
 import { Barchart } from "./components/Barchart";
 import { DropdownAvatar } from "./components/DropdownAvatar";
-import { Room } from "./components/Room";
+import { CreateRoom } from "./components/CreateRoom";
 import { RoomsData } from "./components/RoomsData";
 import { TodoOverview } from "./components/TodoOverview";
 import { Heatmap } from "./components/Heatmap";
@@ -51,7 +51,7 @@ export const Dashboard = () => {
       <ScrollArea>
         <div className="flex gap-4 p-4 sm:flex-row">
           <div className="flex flex-col gap-4 sm:w-1/3">
-            <Room />
+            <CreateRoom />
             <Barchart />
           </div>
           <div className="flex flex-1 flex-col gap-4">
