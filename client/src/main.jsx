82d417dc476444bb4203d4acd23255f69fc414d0
Sourabh Patel <getmeapencil@gmail.com>
2024-09-07 02:30:35 +0530

    Use tooltip provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,13 +5,16 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { ThemeProvider } from "@/components/theme-provider";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>
       <BrowserRouter>
         <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-          <App />
+          <TooltipProvider>
+            <App />
+          </TooltipProvider>
         </ThemeProvider>
       </BrowserRouter>
     </GoogleOAuthProvider>
