4c6346507b8ff3369e961631b695e7b75af75d3e
Sourabh Patel <getmeapencil@gmail.com>
2024-09-18 12:40:42 +0530

    delete

--- client/src/utils/context.js
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from "react";
-
-export const SocketContext = createContext("");
-
-export default function useSocketContext() {
-  return useContext(SocketContext);
-}
