8b0c1c2473d6b9095e2e7a1af0a9fb82b5c7d8be
Sourabh Patel <getmeapencil@gmail.com>
2024-09-20 03:46:54 +0530

    create store for invite

---
+++ client/src/store/invite.js
@@ -0,0 +1,49 @@
+import { create } from "zustand";
+import { createApiCall } from "@/utils/createApiCall";
+
+export const useInvitesStore = create((set, get) => ({
+  invites: [],
+  setInvites: (invite) => {
+    set({ invites: [...get().invites, invite] });
+  },
+  getInvites: async () => {
+    try {
+      const invites = await createApiCall({
+        method: "GET",
+        route: "/invite",
+        withCredentials: true,
+      });
+      set({ invites });
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  acceptInvite: async (inviteId) => {
+    try {
+      await createApiCall({
+        method: "POST",
+        route: "/invite/accept",
+        data: { inviteId },
+        withCredentials: true,
+      });
+      const invites = get().invites.filter((invite) => invite._id !== inviteId);
+      set({ invites: [...invites] });
+    } catch (error) {
+      console.error("Error accepting invite:", error);
+    }
+  },
+  rejectInvite: async (inviteId) => {
+    try {
+      await createApiCall({
+        method: "POST",
+        route: "/invite/reject",
+        data: { inviteId },
+        withCredentials: true,
+      });
+      const invites = get().invites.filter((invite) => invite._id !== inviteId);
+      set({ invites: [...invites] });
+    } catch (error) {
+      console.error("Error rejecting invite:", error);
+    }
+  },
+}));
