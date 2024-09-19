d940c4174808e81d0ec381b03713d204a79eb554
Sourabh Patel <getmeapencil@gmail.com>
2024-09-20 03:49:37 +0530

    use toast to display validation error, don't disable button

--- client/src/pages/dashboard/components/CreateRoom.jsx
+++ client/src/pages/dashboard/components/CreateRoom.jsx
@@ -4,19 +4,25 @@ import { Button } from "@/components/ui/button";
 import Typewriter from "typewriter-effect";
 import { useRoomStore } from "@/store/room";
 import { useState } from "react";
+import { toast } from "sonner";
 
 export const CreateRoom = () => {
   const [name, setName] = useState("");
 
   const createRoom = async () => {
+    if (!name.trim()) {
+      toast.error("Room name can't be empty or whitespace!");
+      return;
+    }
     await useRoomStore.getState().createRoom(name, "");
   };
+
   return (
     <div className="w-full">
       <Card className="min-w-sm">
         <CardHeader>
           <CardTitle>Your Room</CardTitle>
-          <CardDescription className="flex gap-1">
+          <CardDescription className="flex flex-wrap gap-1">
             <span>Create a room of your own and host a</span>
             <Typewriter
               options={{
@@ -36,11 +42,11 @@ export const CreateRoom = () => {
             placeholder="Type room name here"
             aria-label="Text input"
             value={name}
-            onChange={(e) => setName(e.target.value)}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
           />
-          <Button disabled={name.length === 0} onClick={() => createRoom()}>
-            Create New Room
-          </Button>
+          <Button onClick={createRoom}>Create New Room</Button>
         </CardContent>
       </Card>
     </div>
