73cd2e26105b46669db3feb1cde4b631c2073424
Sourabh Patel <getmeapencil@gmail.com>
2024-09-21 16:36:15 +0530

    add error boundary and page

---
+++ client/src/components/ErrorBoundary.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Error } from "@/pages/error";
+
+export class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: null, errorInfo: null };
+  }
+
+  static getDerivedStateFromError() {
+    // Update state so the next render will show the fallback UI
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    console.error("ErrorBoundary caught an error", error, errorInfo);
+    this.setState({ error, errorInfo });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // Fallback UI when an error occurs
+      return <Error />;
+    }
+
+    // If no error, render children components
+    return this.props.children;
+  }
+}
