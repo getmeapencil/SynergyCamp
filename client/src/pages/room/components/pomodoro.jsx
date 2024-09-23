5d6587d72fca11f6417ba9074042a14d892e711b
Sourabh Patel <getmeapencil@gmail.com>
2024-09-24 01:59:21 +0530

    some changes

--- client/src/pages/room/components/pomodoro.jsx
+++ client/src/pages/room/components/pomodoro.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useTimer } from "react-timer-hook";
 import { Play, Pause, Square } from "lucide-react";
 import { Button } from "@/components/ui/button";
@@ -8,13 +8,24 @@ import { Progress } from "@/components/ui/progress";
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 import { InputOTP, InputOTPGroup, InputOTPSlot } from "@/components/ui/input-otp";
 import { Checkbox } from "@/components/ui/checkbox";
+import { usePomodoroStore } from "@/store/pomodoro";
 
 export const Pomodoro = () => {
-  // Default Pomodoro Times: 50 minutes work, 10 minutes break
-  const [workTime, setWorkTime] = useState({ hours: 0, minutes: 0, seconds: 10 });
-  const [breakTime, setBreakTime] = useState({ hours: 0, minutes: 0, seconds: 5 });
-  const [isWorkSession, setIsWorkSession] = useState(true);
-  const [isLooping, setIsLooping] = useState(false);
+  const workTime = usePomodoroStore((state) => state.workTime);
+  const setWorkTime = usePomodoroStore((state) => state.setWorkTime);
+  const breakTime = usePomodoroStore((state) => state.breakTime);
+  const setBreakTime = usePomodoroStore((state) => state.setBreakTime);
+  const isWorkSession = usePomodoroStore((state) => state.isWorkSession);
+  const setIsWorkSession = usePomodoroStore((state) => state.setIsWorkSession);
+  const isLooping = usePomodoroStore((state) => state.isLooping);
+  const setIsLooping = usePomodoroStore((state) => state.setIsLooping);
+  let expiryTime = usePomodoroStore((state) => state.expiryTime);
+  const setExpiryTime = usePomodoroStore((state) => state.setExpiryTime);
+  let isTimerRunning = usePomodoroStore((state) => state.isTimerRunning);
+  const setIsTimerRunning = usePomodoroStore((state) => state.setIsTimerRunning);
+  let pauseTime;
+  console.log("Pomodoro ~ expiryTime:", expiryTime);
+
   const [inputWorkTime, setInputWorkTime] = useState({
     hours: 0,
     minutes: 0,
@@ -30,10 +41,14 @@ export const Pomodoro = () => {
   const calculateDurationInSeconds = ({ hours, minutes, seconds }) => hours * 3600 + minutes * 60 + seconds;
 
   const startDuration = calculateDurationInSeconds(workTime);
-  const expiryTime = new Date();
-  expiryTime.setSeconds(expiryTime.getSeconds() + startDuration);
 
-  const { seconds, minutes, hours, isRunning, pause, resume, restart } = useTimer({
+  if (!expiryTime) {
+    expiryTime = new Date();
+    expiryTime.setSeconds(expiryTime.getSeconds() + startDuration);
+    setExpiryTime(expiryTime);
+  }
+
+  const { seconds, minutes, hours, isRunning, start, pause, resume, restart } = useTimer({
     expiryTimestamp: expiryTime,
     autoStart: false,
     onExpire: () => toggleSession(),
@@ -43,10 +58,11 @@ export const Pomodoro = () => {
   const toggleSession = async () => {
     const nextSession = isWorkSession ? breakTime : workTime;
     const duration = calculateDurationInSeconds(nextSession);
-    const newExpiryTime = await new Date();
-    newExpiryTime.setSeconds(newExpiryTime.getSeconds() + duration);
+    expiryTime = await new Date();
+    expiryTime.setSeconds(expiryTime.getSeconds() + duration);
+    setExpiryTime(expiryTime);
     setIsWorkSession(!isWorkSession);
-    restart(newExpiryTime, true);
+    restart(expiryTime, true);
     if (!isLooping) {
       pause();
     }
@@ -78,21 +94,43 @@ export const Pomodoro = () => {
   };
 
   const stop = () => {
-    const startDuration = calculateDurationInSeconds(inputWorkTime);
-    const time = new Date();
-    time.setSeconds(time.getSeconds() + startDuration);
-    restart(time);
+    const startDuration = calculateDurationInSeconds(workTime);
+    expiryTime = new Date();
+    expiryTime.setSeconds(expiryTime.getSeconds() + startDuration);
+    restart(expiryTime);
     pause();
+    pauseTime = new Date();
+    setIsTimerRunning(false);
   };
+
+  useEffect(() => {
+    if (isTimerRunning) {
+      start();
+    }
+  }, [isTimerRunning, start]);
+
+  useEffect(() => {
+    return () => {
+      console.log("Pomodoro Component is unmounting");
+      const now = new Date();
+      const timeElapsed = now.getTime() - expiryTime.getTime();
+      const newExpiryTime = new Date(timeElapsed + expiryTime.getTime());
+      setExpiryTime(newExpiryTime);
+    };
+  }, [expiryTime, setExpiryTime]);
 
   return (
     <div className="flex flex-1 flex-col gap-4 p-4">
       <Card>
         <CardHeader className="p-4">
           <CardTitle>{isWorkSession ? "Work" : "Break"}</CardTitle>
-          <CardDescription className="flex flex-wrap gap-1">
-            <span>{workTime.minutes} min work,</span>
-            <span>{breakTime.minutes} min break</span>
+          <CardDescription className="flex flex-wrap gap-1 font-mono">
+            <span>
+              {workTime.hours}:{workTime.minutes}:{workTime.seconds} work,
+            </span>
+            <span>
+              {breakTime.hours}:{breakTime.minutes}:{breakTime.seconds} break
+            </span>
           </CardDescription>
         </CardHeader>
         <CardContent className="flex flex-col gap-2 border-t p-4">
@@ -128,7 +166,14 @@ export const Pomodoro = () => {
           {isRunning ? (
             <Tooltip>
               <TooltipTrigger asChild>
-                <Button size="icon" onClick={pause}>
+                <Button
+                  size="icon"
+                  onClick={() => {
+                    pause();
+                    pauseTime = new Date();
+                    setIsTimerRunning(false);
+                  }}
+                >
                   <Pause />
                 </Button>
               </TooltipTrigger>
@@ -137,7 +182,13 @@ export const Pomodoro = () => {
           ) : (
             <Tooltip>
               <TooltipTrigger asChild>
-                <Button size="icon" onClick={resume}>
+                <Button
+                  size="icon"
+                  onClick={() => {
+                    resume();
+                    setIsTimerRunning(true);
+                  }}
+                >
                   <Play />
                 </Button>
               </TooltipTrigger>
