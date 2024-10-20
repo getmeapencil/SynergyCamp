71adea91b8219cbef4843adc732f69fcad85d08e
Sourabh Patel <getmeapencil@gmail.com>
2024-10-20 10:16:14 +0530

    disable history routes in backend

--- server/src/apps/index.js
+++ server/src/apps/index.js
@@ -4,8 +4,9 @@ import roomRouter from "./room/index.js";
 import authRouter from "./auth/index.js";
 import usersRouter from "./users/index.js";
 import inviteRouter from "./invite/index.js";
-import taskRouter from "./task/index.js"
-import historyRouter from "./history/index.js";
+import taskRouter from "./task/index.js";
+// import historyRouter from "./history/index.js";
+
 const apiRouter = express.Router();
 
 apiRouter.use("/room", roomRouter);
@@ -13,6 +14,7 @@ apiRouter.use("/user", userRouter);
 apiRouter.use("/auth", authRouter);
 apiRouter.use("/users", usersRouter);
 apiRouter.use("/invite", inviteRouter);
-apiRouter.use("/task",taskRouter)
-apiRouter.use("/history", historyRouter);
+apiRouter.use("/task", taskRouter);
+// apiRouter.use("/history", historyRouter);
+
 export default apiRouter;
